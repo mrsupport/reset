61ec276603919d8ff446014b9ca45b1f27bd529f
Xin Musa <144815263+mrsupport@users.noreply.github.com>
2023-09-13 20:33:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('reset-form');
-    const emailInput = document.getElementById('email');
-    const resetLinkDiv = document.getElementById('reset-link');
-    const resetLinkText = document.getElementById('reset-link-text');
+   const form = document.getElementById('reset-form');
+   const emailInput = document.getElementById('email');
+   const resetLinkDiv = document.getElementById('reset-link');
+   const resetLinkText = document.getElementById('reset-link-text');
 
-    form.addEventListener('submit', async function (e) {
-        e.preventDefault();
+   form.addEventListener('submit', async function (e) {
+       e.preventDefault();
 
-        const enteredEmail = emailInput.value.trim();
-        if (!enteredEmail) return;
+       const enteredEmail = emailInput.value.trim();
+       if (!enteredEmail) return;
 
-        // Fetch the email and link data from GitHub
-        const githubDataResponse = await fetch(
-            'https://api.github.com/repos/mrsupport/reset/contents/latest-link.txt'
-        );
+       try {
+           // Fetch the email and link data from GitHub
+           const githubDataResponse = await fetch(
+               'https://api.github.com/repos/mrsupport/reset/contents/latest-link.txt'
+           );
 
-        if (githubDataResponse.ok) {
-            const githubData = await githubDataResponse.json();
-            const content = atob(githubData.content);
+           if (githubDataResponse.ok) {
+               const githubData = await githubDataResponse.json();
+               const content = atob(githubData.content);
 
-            // Parse the content as JSON
-            const { email, link } = JSON.parse(content);
+               // Parse the content as JSON
+               const { email, link } = JSON.parse(content);
 
-            // Check if the entered email matches the stored email
-            if (enteredEmail === email) {
-                resetLinkText.textContent = link;
-                resetLinkDiv.style.display = 'block';
-            } else {
-                alert('Email does not match.');
-            }
-        } else {
-            alert('Failed to fetch data from GitHub.');
-        }
-    });
+               // Check if the entered email matches the stored email
+               if (enteredEmail === email) {
+                   resetLinkText.textContent = link;
+                   resetLinkDiv.style.display = 'block';
+               } else {
+                   alert('Email does not match.');
+               }
+           } else {
+               console.error('Failed to fetch data from GitHub:', githubDataResponse.statusText);
+               alert('Failed to fetch data from GitHub.');
+           }
+       } catch (error) {
+           console.error('An error occurred:', error);
+           alert('An error occurred while processing your request.');
+       }
+   });
 });
