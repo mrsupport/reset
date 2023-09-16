318e765e5278e488f489118ea9606e57162ba735
Heath <heathscliff@aol.com>
2023-09-16 14:24:24 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_email = request.form['email']
+
+        # Perform email verification and link retrieval here
+
+        link = None  # Replace with code to fetch the reset link
+
+        if link:
+            return render_template('result.html', link=link)
+        else:
+            return render_template('result.html', message='Email not found.')
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
